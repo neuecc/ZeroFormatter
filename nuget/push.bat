fe749cf5098f714884cc19b7c56d3540aff137f8
neuecc <ils@neue.cc>
2016-10-20 21:25:38 +0900

    complete nuget packages

--- nuget/push.bat
+++ nuget/push.bat
@@ -1,4 +1,4 @@
-nuget push ZeroFormatter.0.1.0.nupkg -Source https://www.nuget.org/api/v2/package
-nuget push ZeroFormatter.Analyzer.1.0.0.0.nupkg -Source https://www.nuget.org/api/v2/package
-nuget push ZeroFormatter.Interfaces.0.1.0.nupkg -Source https://www.nuget.org/api/v2/package
-nuget push ZeroFormatter.Unity.0.1.0.nupkg -Source https://www.nuget.org/api/v2/package
+nuget push ZeroFormatter.0.1.3.nupkg -Source https://www.nuget.org/api/v2/package
+REM nuget push ZeroFormatter.Analyzer.1.0.0.0.nupkg -Source https://www.nuget.org/api/v2/package
+nuget push ZeroFormatter.Interfaces.0.1.3.nupkg -Source https://www.nuget.org/api/v2/package
+nuget push ZeroFormatter.Unity.0.1.3.nupkg -Source https://www.nuget.org/api/v2/package
