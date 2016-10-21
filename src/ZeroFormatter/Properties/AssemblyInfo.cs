deb06218b17333e42ae2670d3a62f9d103626acd
neuecc <ils@neue.cc>
2016-10-21 20:29:32 +0900

    NuGet files update

--- src/ZeroFormatter/Properties/AssemblyInfo.cs
+++ src/ZeroFormatter/Properties/AssemblyInfo.cs
@@ -33,8 +33,8 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("0.1.3.0")]
-[assembly: AssemblyFileVersion("0.1.3.0")]
+[assembly: AssemblyVersion("0.1.4.0")]
+[assembly: AssemblyFileVersion("0.1.4.0")]
 
 
 [assembly: InternalsVisibleTo("ZeroFormatter.Tests")]
