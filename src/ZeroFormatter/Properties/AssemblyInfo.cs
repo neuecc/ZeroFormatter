4fa9cd3f6f87b5a9b98c56d31fe4613a5c27ae2b
neuecc <ils@neue.cc>
2016-11-01 14:34:58 +0900

    0.2.3

--- src/ZeroFormatter/Properties/AssemblyInfo.cs
+++ src/ZeroFormatter/Properties/AssemblyInfo.cs
@@ -33,8 +33,8 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("0.2.2.0")]
-[assembly: AssemblyFileVersion("0.2.2.0")]
+[assembly: AssemblyVersion("0.2.3.0")]
+[assembly: AssemblyFileVersion("0.2.3.0")]
 
 
 [assembly: InternalsVisibleTo("ZeroFormatter.Tests")]
