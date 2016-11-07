d45e6729e2683a998422bf63077c81850387e4eb
neuecc <ils@neue.cc>
2016-11-07 19:25:18 +0900

    PerfTest with JsonUtility

--- tests/ZeroFormatter.UnityTests/Assets/Scripts/Tests/PerformanceTest.cs
+++ tests/ZeroFormatter.UnityTests/Assets/Scripts/Tests/PerformanceTest.cs
@@ -6,20 +6,48 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.Serialization;
 using System.Text;
+using UnityEngine;
 using ZeroFormatter;
 
 namespace ZeroFormatter.Tests
 {
+    [Serializable]
+    public class PersonLike
+    {
+        public int Age;
+        public string FirstName;
+        public string LastName;
+        public Sex2 Sex;
+    }
+
+    [Serializable]
+    public class PersonLikeVector
+    {
+        public PersonLike[] List;
+    }
+
+    public enum Sex2 : int
+    {
+        Unknown = 0,
+        Male = 1,
+        Female = 2
+    }
+
     public class PerformanceTest
     {
-        const int Iteration = 1000; // 1000 iteration.
+        const int Iteration = 500; // 500 iteration.
 
         Person p;
         IList<Person> l;
+        PersonLike p2;
+        PersonLikeVector l2;
+
         byte[] zeroFormatterSingleBytes;
         byte[] zeroFormatterArrayBytes;
         byte[] msgpackSingleBytes;
         byte[] msgpackArrayBytes;
+        byte[] jsonSingleBytes;
+        byte[] jsonArrayBytes;
 
         SerializationContext msgPackContext;
 
@@ -41,7 +69,17 @@ namespace ZeroFormatter.Tests
                 LastName = "Server",
                 Sex = Sex.Male,
             };
+            this.p2 = new PersonLike
+            {
+
+                Age = 99999,
+                FirstName = "Windows",
+                LastName = "Server",
+                Sex = Sex2.Male
+            };
+
             this.l = Enumerable.Range(1000, 1000).Select(x => new Person { Age = x, FirstName = "Windows", LastName = "Server", Sex = Sex.Female }).ToArray();
+            this.l2 = new PersonLikeVector { List = Enumerable.Range(1000, 1000).Select(x => new PersonLike { Age = x, FirstName = "Windows", LastName = "Server", Sex = Sex2.Female }).ToArray() };
 
             zeroFormatterSingleBytes = ZeroFormatterSerializer.Serialize(p);
             zeroFormatterArrayBytes = ZeroFormatterSerializer.Serialize(l);
@@ -49,6 +87,9 @@ namespace ZeroFormatter.Tests
             msgpackSingleBytes = serializer1.PackSingleObject(p);
             var serializer2 = this.msgPackContext.GetSerializer<IList<Person>>();
             msgpackArrayBytes = serializer2.PackSingleObject(l);
+
+            jsonSingleBytes = Encoding.UTF8.GetBytes(JsonUtility.ToJson(p2));
+            jsonArrayBytes = Encoding.UTF8.GetBytes(JsonUtility.ToJson(l2));
         }
 
         private void SerializationContext_ResolveSerializer(object sender, ResolveSerializerEventArgs e)
@@ -124,5 +165,42 @@ namespace ZeroFormatter.Tests
                 serializer.UnpackSingleObject(msgpackArrayBytes);
             }
         }
+
+        public void JsonUtilitySerializeSingle()
+        {
+            for (int i = 0; i < Iteration; i++)
+            {
+                var str = JsonUtility.ToJson(p2);
+                Encoding.UTF8.GetBytes(str); // testing with binary...
+            }
+        }
+
+        public void JsonUtilitySerializeArray()
+        {
+            for (int i = 0; i < Iteration; i++)
+            {
+                var str = JsonUtility.ToJson(l2);
+                Encoding.UTF8.GetBytes(str); // testing with binary...
+            }
+        }
+
+        public void JsonUtilityDeserializeSingle()
+        {
+            for (int i = 0; i < Iteration; i++)
+            {
+                var str = Encoding.UTF8.GetString(jsonSingleBytes);
+                JsonUtility.FromJson<PersonLike>(str);
+            }
+        }
+
+        public void JsonUtilityDeserializeArray()
+        {
+            for (int i = 0; i < Iteration; i++)
+            {
+                var str = Encoding.UTF8.GetString(jsonArrayBytes);
+                JsonUtility.FromJson<PersonLikeVector>(str);
+            }
+        }
+
     }
 }
