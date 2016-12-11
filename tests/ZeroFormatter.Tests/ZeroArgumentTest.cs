8831628a116f18bb698f481378485dca3b6754e6
neuecc <ils@neue.cc>
2016-12-11 16:50:08 +0900

    empty test

---
+++ tests/ZeroFormatter.Tests/ZeroArgumentTest.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZeroFormatter.Tests
+{
+    [ZeroFormattable]
+    public class ZeroClass
+    {
+
+    }
+
+    [ZeroFormattable]
+    public struct ZeroStruct
+    {
+
+    }
+
+    [TestClass]
+    public class ZeroArgumentTest
+    {
+        [TestMethod]
+        public void Class()
+        {
+            ZeroFormatterSerializer.Convert(new ZeroClass()).IsInstanceOf<ZeroClass>();
+        }
+
+        [TestMethod]
+        public void Struct()
+        {
+            ZeroFormatterSerializer.Convert(new ZeroStruct()).IsInstanceOf<ZeroStruct>();
+        }
+    }
+}
